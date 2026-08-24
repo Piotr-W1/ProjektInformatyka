6ce6a2ea2afcffd17b4c2e98fc1e92eba6897ec0
Piotr-W1 <p10trekwaclaw@gmail.com>
2026-08-24 19:05:13 +0200

    Dodanie wyboru pola

--- main.cpp
+++ main.cpp
@@ -2,6 +2,45 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World pierwsza zmiana" << endl;
-    return 0;
+char t[8][8];
+cout << "Saper" <<  endl;
+cout << "Piotr Waclawski" << endl;
+for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+        t[i][j] = '.';
+    }
+}
+cout << "  ";
+for (int i = 0; i < 8; i++) {
+        cout << i+1 << " ";
+}
+cout<< endl;
+for (int i = 0; i < 8; i++) {
+    cout << i+1 << " ";
+    for (int j = 0; j < 8; j++) {
+        cout << t[i][j] << " ";
+    }
+    cout << endl;
+}
+int wiersz, kolumna;
+cout << "Podaj wiersz: ";
+cin >> wiersz;
+cout << "\nPodaj kolumne: ";
+cin >> kolumna;
+cout << endl;
+cout << "Wybrales: " << wiersz << " " << kolumna << endl;
+t[wiersz-1][kolumna-1] = 'X';
+cout << "  ";
+for (int i = 0; i < 8; i++) {
+        cout << i+1 << " ";
+}
+cout<< endl;
+for (int i = 0; i < 8; i++) {
+    cout << i+1 << " ";
+    for (int j = 0; j < 8; j++) {
+        cout << t[i][j] << " ";
+    }
+    cout << endl;
+}
+return 0;
 }
